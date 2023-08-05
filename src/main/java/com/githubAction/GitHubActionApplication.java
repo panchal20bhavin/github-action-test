4e4cdc3ce098d2330b51a37e8aa9a95952f3582e
panchal20bhavin <bhavinpanchal177@gmail.com>
2023-08-05 17:18:27 +0530

    code23

---
+++ src/main/java/com/githubAction/GitHubActionApplication.java
@@ -0,0 +1,13 @@
+package com.githubAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubActionApplication.class, args);
+	}
+
+}
