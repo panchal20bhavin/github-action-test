0a5d693e318f07f74d72a1866eb79c03d12334e4
panchal20bhavin <bhavinpanchal177@gmail.com>
2023-08-05 17:41:58 +0530

    commit cod

--- src/main/java/com/githubAction/GitHubActionApplication.java
+++ src/main/java/com/githubAction/GitHubActionApplication.java
@@ -8,6 +8,7 @@ public class GitHubActionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitHubActionApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
